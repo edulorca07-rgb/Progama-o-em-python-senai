f091977fbbda53f726bc0e17318249be481d4eed
edulorca07-rgb <edu.lorca07@gmail.com>
2026-06-12 17:53:00 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+numero1 = float(input('numero1'))
+numero2= float(input('numero2'))
+
+print (numero1+numero2)
+print (numero1-numero2)
+print (numero1*numero2)
+print (numero1/numero2)
+
+
+
+
+
+
+
+
+
+
+
+
