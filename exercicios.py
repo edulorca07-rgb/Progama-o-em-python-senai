7fe7aca1e367eeae61815bdca0172665b0b602b6
edulorca07-rgb <edu.lorca07@gmail.com>
2026-06-17 15:40:25 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,22 @@
+print('sejam bem vindos')
+valor = (10>5)
+print(valor)
+resultado = (5.69*5,0)
+print(resultado)
+print(12/2)
+print(6-3)
+print(40/2/2)
+print(0.2*0.4*0.7*0.8)
+
+numero = 5
+print(numero)
+print(numero*2)
+nome = input('nome:')
+email = input('email:')
+idade = input('idade:')
+cidade = input('cidade:')
+graduação = input ('graduação:')
+estado_civil = input('estado civil:')
+print(nome, email, idade, cidade, graduação, estado_civil)
+
+
