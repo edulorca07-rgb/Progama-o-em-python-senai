c36b4d50b474f76cf6778419b3a64fbf33dcb695
kakashi21423 <edu.lorca07@gmail.com>
2026-06-18 16:29:02 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,26 @@
+for i in range(2,21,2):
+    print(i)
+    
+    
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros)
+
+
+print(numeros[2])
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10]
+
+print(numeros)
+
+numeros = [1, 2, 3, 4, 6, 7, 8, 9, 9, 10]
+
+
+print(numeros)
+
+
+carros = ['Fusca', 'corola', 'civic']
+resultado = carros + numeros
+print(resultado)
+
+
