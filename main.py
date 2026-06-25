b6d65bc07faada1040978a5036c7196f74fce287
eduardo <edu.lorca07@gmail.com>
2026-06-25 15:03:02 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,21 @@
-numero = float(input("Digite um número: "))
+contador = 0
 
-if numero > 0:
-    print("O número é positivo.")
-elif numero < 0:
-    print("O número é negativo.")
-else:
-    print("O número é zero.")
-    
-
-idade = int(input("Digite a sua idade: "))
-
-if idade < 16:
-    print("Você ainda não pode votar.")
-elif 18 <= idade <= 70:
-    print("O voto é obrigatório.")
-else:
-    print("O voto é opcional")
-
-numero = 15
+while contador <= 1000:
+    print(contador)
+    contador += 1
 
 
-if numero % 2 == 0:
-    print(f"O número {numero} é par.")
-else:
-    print(f"O número {numero} é ímpar.")
+    nomes = []
+contador = 0
 
 
-lado1 = float(input("Digite o primeiro lado: "))
-lado2 = float(input("Digite o segundo lado: "))
-lado3 = float(input("Digite o terceiro lado: "))
+while contador < 10:
+    nome = input(f"Digite o nome da {contador + 1}ª pessoa: ")
+    nomes.append(nome)
+    contador += 1
 
 
-if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
-
-    if lado1 == lado2 == lado3:
-        print("O triângulo é Equilátero.")
-    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-        print("O triângulo é Isósceles.")
-    else:
-        print("O triângulo é Escaleno.")
-
-
-def verificar_multiplo_v1(numero):
-    if numero % 5 == 0 and numero % 7 == 0:
-        return f"O número {numero} é múltiplo de 5 e 7."
-    else:
-        return f"O número {numero} NÃO é múltiplo de 5 e 7."
-   
-   
-   
-numero = 15
-
-if numero > 0 and numero > 10:
-    print("O número é positivo e maior que 10.")
-else:
-    print("O número não atende aos requisitos.")
-
-
-numero = int(input("Digite um número: "))
-
-if numero % 3 == 0 or numero % 5 == 0:
-    print(f"O número {numero} é divisível por 3 ou por 5.")
-else:
-    print(f"O número {numero} NÃO é divisível por 3 nem por 5.")
-    
+print("\n--- Pessoas Cadastradas ---")
+for n in range (10):
+    nome = input ('nome:')
+    print(nome)
